fb9f2dd5e37af2de75827798139fc1524ab7c0ed
Yaozhu Sun <yzyzsun@gmail.com>
2020-06-10 19:00:03 +0800

    Add lezer parser and its syntax highlight

--- plground/index.ts
+++ plground/index.ts
@@ -2,14 +2,62 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { EditorView } from '@codemirror/next/view';
-import { EditorState } from '@codemirror/next/state';
+import { EditorState, languageData } from '@codemirror/next/state';
 import { keymap } from '@codemirror/next/keymap';
 import { baseKeymap } from '@codemirror/next/commands';
 import { lineNumbers } from '@codemirror/next/gutter';
 import { foldGutter } from '@codemirror/next/fold';
 import { bracketMatching } from '@codemirror/next/matchbrackets';
 import { closeBrackets } from '@codemirror/next/closebrackets';
-import { defaultHighlighter } from '@codemirror/next/highlight';
+import { defaultHighlighter, styleTags } from '@codemirror/next/highlight';
+import { LezerSyntax, continuedIndent, indentNodeProp, foldNodeProp } from '@codemirror/next/syntax';
+import { Subtree } from 'lezer';
+import { buildParser } from 'lezer-generator';
+import grammar from 'raw-loader!./zord.grammar';
+
+const parser = buildParser(grammar);
+const zordSyntax = new LezerSyntax(parser.withProps(
+  languageData.add({
+    Program: {
+      closeBrackets: { brackets: ['{', '(', '[', '<', '"'] },
+      commentTokens: { line: "--", block: { open: "{-", close: "-}" } },
+    }
+  }),
+  indentNodeProp.add({
+    RecordType: continuedIndent(),
+    Record: continuedIndent(),
+  }),
+  foldNodeProp.add({
+    RecordType(tree: Subtree) { return {from: tree.start + 1, to: tree.end - 1} },
+    Record(tree: Subtree) { return {from: tree.start + 1, to: tree.end - 1} },
+  }),
+  styleTags({
+    'type extends let letrec override trait implements inherits': 'keyword definition',
+    'if then else new open in forall': 'keyword',
+    'Num Bool String Top Bot Trait': 'keyword',
+    Boolean: 'atom',
+    Undefined: 'null',
+    Unit: 'unit',
+    TermName: 'variableName',
+    TermNameDef: 'variableName definition',
+    LabelDecl: 'propertyName definition',
+    LineComment: 'lineComment',
+    BlockComment: 'blockComment',
+    Number: 'number',
+    String: 'string',
+    TypeOp: 'typeOperator',
+    ArithOp: 'arithmeticOperator',
+    LogicOp: 'logicOperator',
+    CompareOp: 'compareOperator',
+    MergeOp: 'operator',
+    '( )': 'paren',
+    '{ }': 'brace',
+    '[ ]': 'squareBracket',
+    '< >': 'angleBracket',
+    '.': 'derefOperator',
+    ', ;': 'separator',
+  })
+));
 
 const state = EditorState.create({
   extensions: [
@@ -19,6 +67,7 @@ const state = EditorState.create({
     bracketMatching(),
     closeBrackets,
     defaultHighlighter,
+    zordSyntax.extension,
   ],
 });
 const view = new EditorView({ state });
