357110042d7db1bc5bf46eed0feee9fa6fccf948
JimmyZJX <x@201a.cn>
2020-07-16 19:00:44 +0800

    UI; inject js to head

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: './plground/index.html' }),
+    new HtmlWebpackPlugin({ template: './plground/index.html', inject: 'head' }),
   ],
   devtool: 'inline-source-map',
   output: {
