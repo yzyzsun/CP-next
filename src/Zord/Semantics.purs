027b5a3f40fddfed86bb2542bddf1ce0ed907c60
Yaozhu Sun <yzyzsun@gmail.com>
2020-08-28 16:12:27 +0800

    Eliminate redundant type annotations during evaluation

--- src/Zord/Semantics.purs
+++ src/Zord/Semantics.purs
@@ -28,28 +28,37 @@ eval e | isValue e = tell (show e <> "\n") $> e
        | otherwise = tell (show e <> "\n") *> step e >>= eval
 
 step :: Tm -> Eval Tm
-step (TmUnary op e) | isValue e = computation "UnaryV" $> unop op e
-                    | otherwise = congruence "Unary" $> TmUnary op <*> step e
-step (TmBinary op e1 e2) | isValue e1 && isValue e2 = computation "BinaryV" $> binop op e1 e2
-                         | isValue e1 = congruence "BinaryR" $> TmBinary op e1 <*> step e2
-                         | otherwise  = congruence "BinaryL" $> TmBinary op <*> step e1 <@> e2
+step (TmUnary op e)
+  | isValue e = computation "UnaryV" $> unop op e
+  | otherwise = congruence  "Unary"  $> TmUnary op <*> step e
+step (TmBinary op e1 e2)
+  | isValue e1 && isValue e2 = computation "BinaryV" $> binop op e1 e2
+  | isValue e1 = congruence "BinaryR" $> TmBinary op e1 <*> step e2
+  | otherwise  = congruence "BinaryL" $> TmBinary op <*> step e1 <@> e2
 step (TmIf (TmBool true)  e2 e3) = computation "IfTrue"  $> e2
 step (TmIf (TmBool false) e2 e3) = computation "IfFalse" $> e3
 step (TmIf e1 e2 e3) = congruence "If" $> TmIf <*> step e1 <@> e2 <@> e3
-step (TmApp e1 e2) | isValue e1 = computation "PApp" $> paraApp e1 e2
-                   | otherwise  = congruence "AppL" $> TmApp <*> step e1 <@> e2
+step (TmApp e1 e2)
+  | isValue e1 = computation "PApp" $> paraApp e1 e2
+  | otherwise  = congruence  "AppL" $> TmApp <*> step e1 <@> e2
 step (TmFix x e t) = computation "Fix" $> TmAnno (tmSubst x (TmFix x e t) e) t
-step (TmAnno e t) | isValue e = computation "AnnoV" $> fromJust (typedReduce e t)
-                  | otherwise = congruence "Anno" $> TmAnno <*> step e <@> t
-step (TmMerge e1 e2) | isValue e1 = congruence "MergeR" $> TmMerge e1 <*> step e2
-                     | isValue e2 = congruence "MergeL" $> TmMerge <*> step e1 <@> e2
-                     | otherwise  = congruence "Merge"  $> TmMerge <*> step e1 <*> step e2
-step (TmPrj e l) | isValue e = computation "PProj" $> selectLabel e l
-                 | otherwise = congruence "Proj" $> TmPrj <*> step e <@> l
-step (TmTApp e t) | isValue e = computation "PTApp" $> paraApp' e t
-                  | otherwise = congruence "TApp" $> TmTApp <*> step e <@> t
-step (TmToString e) | isValue e = computation "ToStringV" $> toString e
-                    | otherwise = congruence "ToString" $> TmToString <*> step e
+step (TmAnno (TmAnno e t') t) = computation "AnnoAnno" $> TmAnno e t
+step (TmAnno e t)
+  | isValue e = computation "AnnoV" $> fromJust (typedReduce e t)
+  | otherwise = congruence  "Anno"  $> TmAnno <*> step e <@> t
+step (TmMerge e1 e2)
+  | isValue e1 = congruence "MergeR" $> TmMerge e1 <*> step e2
+  | isValue e2 = congruence "MergeL" $> TmMerge <*> step e1 <@> e2
+  | otherwise  = congruence "Merge"  $> TmMerge <*> step e1 <*> step e2
+step (TmPrj e l)
+  | isValue e = computation "PProj" $> selectLabel e l
+  | otherwise = congruence  "Proj"  $> TmPrj <*> step e <@> l
+step (TmTApp e t)
+  | isValue e = computation "PTApp" $> paraApp' e t
+  | otherwise = congruence  "TApp"  $> TmTApp <*> step e <@> t
+step (TmToString e)
+  | isValue e = computation "ToStringV" $> toString e
+  | otherwise = congruence  "ToString"  $> TmToString <*> step e
 step e = unsafeCrashWith $
   "Zord.Semantics.step: well-typed programs don't get stuck, but got " <> show e
 
