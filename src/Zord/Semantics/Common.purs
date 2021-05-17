8588130697f6f292f1c1d9fe9b36f602c29077a8
Yaozhu Sun <yzyzsun@gmail.com>
2021-05-17 20:31:48 +0800

    Fix IncompleteExhaustivityCheck warnings

--- src/Zord/Semantics/Common.purs
+++ src/Zord/Semantics/Common.purs
@@ -19,49 +19,64 @@ unop op v = unsafeCrashWith $
   " on " <> show v
 
 binop :: BinOp -> Tm -> Tm -> Tm
-binop (Arith Add) (TmInt i1) (TmInt i2) = TmInt (i1 + i2)
-binop (Arith Sub) (TmInt i1) (TmInt i2) = TmInt (i1 - i2)
-binop (Arith Mul) (TmInt i1) (TmInt i2) = TmInt (i1 * i2)
-binop (Arith Div) (TmInt i1) (TmInt i2) = TmInt (i1 / i2)
-binop (Arith Mod) (TmInt i1) (TmInt i2) = TmInt (i1 `mod` i2)
-binop (Arith Add) (TmDouble n1) (TmDouble n2) = TmDouble (n1 + n2)
-binop (Arith Sub) (TmDouble n1) (TmDouble n2) = TmDouble (n1 - n2)
-binop (Arith Mul) (TmDouble n1) (TmDouble n2) = TmDouble (n1 * n2)
-binop (Arith Div) (TmDouble n1) (TmDouble n2) = TmDouble (n1 / n2)
-binop (Arith Mod) (TmDouble n1) (TmDouble n2) = TmDouble (n1 % n2)
-binop (Comp Eql) (TmInt i1) (TmInt i2) = TmBool (i1 == i2)
-binop (Comp Neq) (TmInt i1) (TmInt i2) = TmBool (i1 /= i2)
-binop (Comp Lt ) (TmInt i1) (TmInt i2) = TmBool (i1 <  i2)
-binop (Comp Le ) (TmInt i1) (TmInt i2) = TmBool (i1 <= i2)
-binop (Comp Gt ) (TmInt i1) (TmInt i2) = TmBool (i1 >  i2)
-binop (Comp Ge ) (TmInt i1) (TmInt i2) = TmBool (i1 >= i2)
-binop (Comp Eql) (TmDouble n1) (TmDouble n2) = TmBool (n1 == n2)
-binop (Comp Neq) (TmDouble n1) (TmDouble n2) = TmBool (n1 /= n2)
-binop (Comp Lt ) (TmDouble n1) (TmDouble n2) = TmBool (n1 <  n2)
-binop (Comp Le ) (TmDouble n1) (TmDouble n2) = TmBool (n1 <= n2)
-binop (Comp Gt ) (TmDouble n1) (TmDouble n2) = TmBool (n1 >  n2)
-binop (Comp Ge ) (TmDouble n1) (TmDouble n2) = TmBool (n1 >= n2)
-binop (Comp Eql) (TmString s1) (TmString s2) = TmBool (s1 == s2)
-binop (Comp Neq) (TmString s1) (TmString s2) = TmBool (s1 /= s2)
-binop (Comp Lt ) (TmString s1) (TmString s2) = TmBool (s1 <  s2)
-binop (Comp Le ) (TmString s1) (TmString s2) = TmBool (s1 <= s2)
-binop (Comp Gt ) (TmString s1) (TmString s2) = TmBool (s1 >  s2)
-binop (Comp Ge ) (TmString s1) (TmString s2) = TmBool (s1 >= s2)
-binop (Comp Eql) (TmBool b1) (TmBool b2) = TmBool (b1 == b2)
-binop (Comp Neq) (TmBool b1) (TmBool b2) = TmBool (b1 /= b2)
-binop (Comp Lt ) (TmBool b1) (TmBool b2) = TmBool (b1 <  b2)
-binop (Comp Le ) (TmBool b1) (TmBool b2) = TmBool (b1 <= b2)
-binop (Comp Gt ) (TmBool b1) (TmBool b2) = TmBool (b1 >  b2)
-binop (Comp Ge ) (TmBool b1) (TmBool b2) = TmBool (b1 >= b2)
-binop (Logic And) (TmBool b1) (TmBool b2) = TmBool (b1 && b2)
-binop (Logic Or ) (TmBool b1) (TmBool b2) = TmBool (b1 || b2)
-binop Append (TmString s1) (TmString s2) = TmString (s1 <> s2)
+binop (Arith op) v1 v2 = arith op v1 v2
+binop (Comp  op) v1 v2 = comp  op v1 v2
+binop (Logic op) v1 v2 = logic op v1 v2
+binop Append (TmString s1)  (TmString s2)  = TmString (s1 <> s2)
 binop Append (TmArray t l1) (TmArray _ l2) = TmArray t (l1 <> l2)
 binop Index (TmArray t arr) (TmInt i) = case arr !! i of
   Just e -> TmAnno e t
   Nothing -> unsafeCrashWith $ "Zord.Semantics.Common.binop: the index " <>
     show i <> " is out of bounds for " <> show (TmArray t arr)
-binop op v1 v2 = unsafeCrashWith $
+binop op v1 v2 = unsafeCrashWithBinop op v1 v2
+
+arith :: ArithOp -> Tm -> Tm -> Tm
+arith Add (TmInt i1) (TmInt i2) = TmInt (i1 + i2)
+arith Sub (TmInt i1) (TmInt i2) = TmInt (i1 - i2)
+arith Mul (TmInt i1) (TmInt i2) = TmInt (i1 * i2)
+arith Div (TmInt i1) (TmInt i2) = TmInt (i1 / i2)
+arith Mod (TmInt i1) (TmInt i2) = TmInt (i1 `mod` i2)
+arith Add (TmDouble n1) (TmDouble n2) = TmDouble (n1 + n2)
+arith Sub (TmDouble n1) (TmDouble n2) = TmDouble (n1 - n2)
+arith Mul (TmDouble n1) (TmDouble n2) = TmDouble (n1 * n2)
+arith Div (TmDouble n1) (TmDouble n2) = TmDouble (n1 / n2)
+arith Mod (TmDouble n1) (TmDouble n2) = TmDouble (n1 % n2)
+arith op v1 v2 = unsafeCrashWithBinop (Arith op) v1 v2
+
+comp :: CompOp -> Tm -> Tm -> Tm
+comp Eql (TmInt i1) (TmInt i2) = TmBool (i1 == i2)
+comp Neq (TmInt i1) (TmInt i2) = TmBool (i1 /= i2)
+comp Lt  (TmInt i1) (TmInt i2) = TmBool (i1 <  i2)
+comp Le  (TmInt i1) (TmInt i2) = TmBool (i1 <= i2)
+comp Gt  (TmInt i1) (TmInt i2) = TmBool (i1 >  i2)
+comp Ge  (TmInt i1) (TmInt i2) = TmBool (i1 >= i2)
+comp Eql (TmDouble n1) (TmDouble n2) = TmBool (n1 == n2)
+comp Neq (TmDouble n1) (TmDouble n2) = TmBool (n1 /= n2)
+comp Lt  (TmDouble n1) (TmDouble n2) = TmBool (n1 <  n2)
+comp Le  (TmDouble n1) (TmDouble n2) = TmBool (n1 <= n2)
+comp Gt  (TmDouble n1) (TmDouble n2) = TmBool (n1 >  n2)
+comp Ge  (TmDouble n1) (TmDouble n2) = TmBool (n1 >= n2)
+comp Eql (TmString s1) (TmString s2) = TmBool (s1 == s2)
+comp Neq (TmString s1) (TmString s2) = TmBool (s1 /= s2)
+comp Lt  (TmString s1) (TmString s2) = TmBool (s1 <  s2)
+comp Le  (TmString s1) (TmString s2) = TmBool (s1 <= s2)
+comp Gt  (TmString s1) (TmString s2) = TmBool (s1 >  s2)
+comp Ge  (TmString s1) (TmString s2) = TmBool (s1 >= s2)
+comp Eql (TmBool b1) (TmBool b2) = TmBool (b1 == b2)
+comp Neq (TmBool b1) (TmBool b2) = TmBool (b1 /= b2)
+comp Lt  (TmBool b1) (TmBool b2) = TmBool (b1 <  b2)
+comp Le  (TmBool b1) (TmBool b2) = TmBool (b1 <= b2)
+comp Gt  (TmBool b1) (TmBool b2) = TmBool (b1 >  b2)
+comp Ge  (TmBool b1) (TmBool b2) = TmBool (b1 >= b2)
+comp op v1 v2 = unsafeCrashWithBinop (Comp op) v1 v2
+
+logic :: LogicOp -> Tm -> Tm -> Tm
+logic And (TmBool b1) (TmBool b2) = TmBool (b1 && b2)
+logic Or  (TmBool b1) (TmBool b2) = TmBool (b1 || b2)
+logic op v1 v2 = unsafeCrashWithBinop (Logic op) v1 v2
+
+unsafeCrashWithBinop :: forall a. BinOp -> Tm -> Tm -> a
+unsafeCrashWithBinop op v1 v2 = unsafeCrashWith $
   "Zord.Semantics.Common.binop: impossible binary operation " <> show op <>
   " between " <> show v1 <> " and " <> show v2
 
