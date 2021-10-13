181712e7f86b4c27f9ae5858d12ed2d2ed923a02
Yaozhu Sun <yzyzsun@gmail.com>
2021-10-14 05:25:45 +0800

    Go back to referer after logging in / out

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
-  protected
+  private
     def after_sign_in_path_for(resource)
-      "/#{resource.username}"
+      if request.referer.end_with? new_user_session_path
+        "/#{resource.username}"
+      else
+        request.referer
+      end
+    end
+    def after_sign_out_path_for(resource_or_scope)
+      request.referer
     end
 end
